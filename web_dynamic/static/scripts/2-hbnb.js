5408bba94dd2592dc6b92b23cb6a5c3babea1b94
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-03-03 15:23:53 +0100

    Update header styles and checkbox functionality

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,21 @@
 $("document").ready(function() {
-    const url = "http://0.0.0.0:5001/api/v1/status/";
-    $.get(url, function(data, textStatus) {
+    const amenities = {};
+    $('input[type="checkbox"]').change(function() {
+        if ($(this).is(':checked')) {
+            amenities[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete amenities[$(this).data('id')];
+        }
+        const amenityList = Object.values(amenities);
+        if (amenityList.length > 0) {
+            $('.amenities h4').text(amenityList.join(', '));
+        } else {
+            $('.amenities h4').html('&nbsp;');
+        }
+    });
+
+    const urlStatus = "http://0.0.0.0:5001/api/v1/status/";
+    $.get(urlStatus, function(data, textStatus) {
         if (textStatus === "success") {
             $("#api_status").addClass("available");
         }
---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,72 @@
+$("document").ready(function() {
+    const amenities = {};
+    $('input[type="checkbox"]').change(function() {
+		console.log(amenities)
+        if ($(this).is(':checked')) {
+            amenities[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete amenities[$(this).data('id')];
+        }
+        const amenityList = Object.values(amenities);
+        if (amenityList.length > 0) {
+            $('.amenities h4').text(amenityList.join(', '));
+        } else {
+            $('.amenities h4').html('&nbsp;');
+        }
+    });
+
+    const urlStatus = "http://0.0.0.0:5001/api/v1/status/";
+    $.get(urlStatus, function(data, textStatus) {
+        if (textStatus === "success") {
+            $("#api_status").addClass("available");
+        }
+        else {
+            $("#api_status").removeClass("available");
+        }
+    });
+
+	const searchPlaces = (amenities={})=> {
+		const urlPlaces = "http://0.0.0.0:5001/api/v1/places_search/"
+		$.post({
+			url: urlPlaces,
+			data: JSON.stringify(amenities),
+			headers: {
+				"Content-Type": "application/json",
+			},
+			success: (places) => {
+				places.forEach((place) =>
+					$("section.places").append(
+						`<article>
+				<div class="title_box">
+				<h2>${place.name}</h2>
+				<div class="price_by_night">$${place.price_by_night}</div>
+				</div>
+				<div class="information">
+				<div class="max_guest">${place.max_guest} Guest${
+							place.max_guest !== 1 ? "s" : ""
+						}</div>
+				<div class="number_rooms">${place.number_rooms} Bedroom${
+							place.number_rooms !== 1 ? "s" : ""
+						}</div>
+				<div class="number_bathrooms">${place.number_bathrooms} Bathroom${
+							place.number_bathrooms !== 1 ? "s" : ""
+						}</div>
+				</div> 
+				<div class="description">
+				${place.description}
+				</div>
+					</article>`
+					)
+				);
+			}
+		});
+	}
+	searchPlaces();
+
+	$("button").click(function() {
+		$("section.places").empty();
+
+		searchPlaces({amenities:Object.keys(amenities)});
+	}
+	);
+});
